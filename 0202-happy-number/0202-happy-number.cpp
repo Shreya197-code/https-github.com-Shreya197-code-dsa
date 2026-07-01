bdff4e4d78b5e50c5b5c49cc54a26ba10e992217
Shreya Sah <shreyashahi197@gmail.com>
2026-07-02 00:26:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.54%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+int total(int n){
+    int sum=0;
+    while(n>0){
+        int ld=n%10;
+        sum+= ld*ld;
+        n=n/10;
+    }
+    return sum;
+}
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+
+      do{
+        slow=total(slow);
+        fast=total(total(fast));
+      }
+      while(slow!=fast);
+      return slow==1;
+        
+    }
+};
