20aa44430584182524cdc2e31d1d84a117fec742
Shreya Sah <shreyashahi197@gmail.com>
2026-07-09 13:02:17 +0530

    Time: 18 ms (42.87%), Space: 78.4 MB (27.07%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+     vector<int>diff(n,0);
+     for(auto book:bookings){
+        int first=book[0];
+        int last=book[1];
+        int seats=book[2];
+  diff[first-1]+=seats;
+  if(last<n){
+    diff[last]-=seats;
+  }
+     }
+     for(int i=1;i<n;i++){
+        diff[i]+=diff[i-1];
+     }
+
+     return diff;
+    }
+};
