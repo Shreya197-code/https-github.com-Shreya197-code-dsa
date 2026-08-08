648f56be12c4f1691ebdb6ec23fd464d7d31fe6c
Shreya Sah <shreyashahi197@gmail.com>
2026-08-09 02:23:14 +0530

    Time: 16 ms (58.81%), Space: 26.1 MB (49.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    unordered_map<string,vector<string>>mpp;
-    for(string s:strs){
-       string temp=s;
-       sort(temp.begin(),temp.end());
-       mpp[temp].push_back(s);
-    }
-    vector<vector<string>>ans;
-    for(auto it:mpp){
-        ans.push_back(it.second);
-    }
-    return ans;
+        unordered_map<string ,vector<string>>mpp;
+        for(int i=0;i<strs.size();i++){
+            string key=strs[i];
+            sort(key.begin(),key.end());
+            mpp[key].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+        for(auto x:mpp){
+            ans.push_back(x.second);
+        }
+        return ans;
     }
 };
