b0f3f40ab5fa706493eeed82b47a117f424e7683
Shreya Sah <shreyashahi197@gmail.com>
2026-08-15 12:17:19 +0530

    Time: 43 ms (83.5%), Space: 29.1 MB (73.36%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
         sort(nums.begin(), nums.end());
-
-        vector<vector<int>> ans;
-        int n = nums.size();
-
-        for(int i = 0; i < n - 2; i++) {
-
-            // Skip duplicate first elements
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-
-                    ans.push_back({nums[i], nums[left], nums[right]});
-
-                    left++;
-                    right--;
-
-                    while(left < right && nums[left] == nums[left - 1])
-                        left++;
-
-                    while(left < right && nums[right] == nums[right + 1])
-                        right--;
-                }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
-            }
+           vector<vector<int>>ans;
+        for(int i = 0; i < nums.size(); i++){
+              if(i > 0 && nums[i] == nums[i-1])
+    continue;
+    int j = i + 1;
+    int k = nums.size() - 1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+         if(sum==0){
+           ans.push_back({nums[i],nums[j],nums[k]});
+           j++;
+           k--;
+        while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
+         }
+         
+         else if(sum<0){
+            j++;
+         }
+         else {
+            k--;
+         }
         }
-
-        return ans;
+        }
+return ans;
     }
-};
+    };
