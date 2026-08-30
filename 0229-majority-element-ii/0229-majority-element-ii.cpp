77cbaaa6110bff395c7b0fbb85b3ba221dcbc315
Shreya Sah <shreyashahi197@gmail.com>
2026-08-30 18:02:48 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.42%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-      int cand1;
-      int cand2;
-      int count1=0;
-      int count2=0;
-      int n=nums.size();
-      for(int x:nums){
-        if(x==cand1){
+           int n=nums.size();
+     int cand1=0;
+     int cand2=0;
+     int count1=0;
+     int count2=0;
+     for(int i=0;i<n;i++){
+        if(nums[i]==cand1){
             count1++;
         }
-        else if(x==cand2){
+       else if(nums[i]==cand2){
             count2++;
         }
         else if(count1==0){
-            cand1=x;
+            cand1=nums[i];
             count1++;
         }
         else if(count2==0){
-            cand2=x;
+            cand2=nums[i];
             count2++;
         }
         else{
             count1--;
             count2--;
         }
-      }
-      count1=0;
-      count2=0;
-      vector<int>ans;
-      for(int x:nums){
-        if(x==cand1){
-            count1++;
-        }
-     else if(x==cand2){
-            count2++;
-        }
-      }
-      if(count1>n/3){
-        ans.push_back(cand1);
-      }
-      if(count2>n/3){
-        ans.push_back(cand2);
-      }
-      return ans;
+     }
+     count1=0;
+     count2=0;
+     vector<int>ans;
+     for(int i=0;i<n;i++){
+        if(nums[i]==cand1)count1++;
+        else if(nums[i]==cand2)
+             count2++;
+     }
+     if(count1>n/3) ans.push_back(cand1);
+      if(count2>n/3) ans.push_back(cand2);
+     return ans;
 
     }
 };
